85503e352759eed680546d608e9e03a937917901
“Teo” <matiussw@gmail.com>
2026-02-19 19:01:59 -0500

    Ejercicios Condicionales

--- src/src/EjerciciosCondicionales/EjerciciosCondicionales.java
+++ src/src/EjerciciosCondicionales/EjerciciosCondicionales.java
@@ -175,6 +175,38 @@ public class EjerciciosCondicionales {
                 System.out.println("Numero Ingresado es Cero "+numero);
             }
 
+            if(Ejercicios==2){
+                //Inicio Desarrollo Ejercicio 2
+            }
+
+            if(Ejercicios==3){
+                //Inicio Desarrollo Ejercicio 3
+            }
+            if(Ejercicios==4){
+                //Inicio Desarrollo Ejercicio 4
+            }
+            if(Ejercicios==5){
+                //Inicio Desarrollo Ejercicio 5
+            }
+            if(Ejercicios==6){
+                //Inicio Desarrollo Ejercicio 6
+            }
+            if(Ejercicios==7){
+                //Inicio Desarrollo Ejercicio 7
+            }
+            if(Ejercicios==8){
+                //Inicio Desarrollo Ejercicio 8
+            }
+
+            if(Ejercicios==9){
+                //Inicio Desarrollo Ejercicio 9
+            }
+            if(Ejercicios==10){
+                //Inicio Desarrollo Ejercicio 10
+            }
+
+
+
         }
 
 
